c1bafcc875bf2817f44603f3917ab736698ca90c
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-04-24 21:10:27 -0300

    salvando string e esboçando função de lopping

--- forca.c
+++ forca.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    char palavrasecreta[20];
 
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    /*do {
+
+    } while(!acertou && !enforcou){
+
+    }*/
+
+    return 0;
 }
